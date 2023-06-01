fa0135144efee101bcf76f9c169f32a80de78ac0
LilachD <lilacd@gmail.com>
2023-06-01 17:30:45 +0100

    neighbours movement, updated svgs

--- app.js
+++ app.js
@@ -1,30 +1,55 @@
 const grid = document.querySelector('.grid')
 const horn = document.querySelector('.horn')
-let currentHornIndex = 162
-let width = 12
+let currentHornIndex = 195
+let width = 14
+let direction = 1
+let goingRight = true
+let neighboursId;
 
-for (let i = 0; i < 168; i++) {
+for (let i = 0; i < 196; i++) {
     const square = document.createElement('div');
-    grid.appendChild(square);  
+    grid.appendChild(square);
 }
 
 const spaces = Array.from(document.querySelectorAll('.grid div'))
 
-const neighbours = [
+const angry = [
     0,1,2,3,4,5,6,7,8,9,
-    12,13,14,15,16,17,18,19,20,21,
-    24,25,26,27,28,29,30,31,32,33,
-    36,37,38,39,40,41,42,43,44,45,
-    48,49,50,51,52,53,54,55,56,57
+    14,23,
+    28,29,34,35,36,37,
+    42,43,44,45,46,47,48,49,50,51,
+    56,57,58,59,60,61,62,63,64,65
 ]
+const vAngry = [15,16,21,22,30,31,34,35]
+const xAngry = [17,18,19,20,32,33]
+
+//const neighbours = angry.concat(vAngry, xAngry).sort((a,b) => (a-b));
 
 function draw() {
-    for(i in neighbours) {
-      spaces[neighbours[i]].classList.add('neighbour'); 
-    }
+    for (let i = 0; i < angry.length; i++) {
+      spaces[angry[i]].classList.add('angry'); 
+    };
+    for (let i = 0; i < vAngry.length; i++) {
+        spaces[vAngry[i]].classList.add('v-angry');
+    };
+    for (let i = 0; i < xAngry.length; i++) {
+        spaces[xAngry[i]].classList.add('x-angry');
+    };
 }
 
 draw()
+
+function remove() {
+    for (let i = 0; i < angry.length; i++) {
+        spaces[angry[i]].classList.remove('angry'); 
+      };
+      for (let i = 0; i < vAngry.length; i++) {
+          spaces[vAngry[i]].classList.remove('v-angry');
+      };
+      for (let i = 0; i < xAngry.length; i++) {
+          spaces[xAngry[i]].classList.remove('x-angry');
+      };
+}
 
 spaces[currentHornIndex].classList.add('horn')
 
@@ -42,3 +67,57 @@ function moveHorn(e) {
 }
 
 document.addEventListener('keydown', moveHorn)
+
+function moveNeighbours()  {
+    const leftEdge = angry[0] % width === 0;
+    const rightEdge = angry[angry.length-1] % width === (width - 1);
+    remove ();
+
+    if (rightEdge && goingRight) {
+        direction = -1;
+        goingRight = false;
+        for (let i = 0; i < angry.length; i++) {
+            angry[i] += width + 1;
+        };
+        for (let i = 0; i < vAngry.length; i++) {
+            vAngry[i] += width + 1;
+        };
+        for (let i = 0; i < xAngry.length; i++) {
+            xAngry[i] += width + 1;
+        }; 
+    }
+
+    if (leftEdge && !goingRight) {
+        direction = 1;
+        goingRight = true;
+        for (let i = 0; i < angry.length; i++) {
+            angry[i] += width - 1;
+        };
+        for (let i = 0; i < vAngry.length; i++) {
+            vAngry[i] += width - 1;
+        };
+        for (let i = 0; i < xAngry.length; i++) {
+            xAngry[i] += width - 1;
+        };     
+    }
+
+    for (let i = 0; i < angry.length; i++) {
+        angry[i] += direction;
+    };
+    for (let i = 0; i < vAngry.length; i++) {
+        vAngry[i] += direction;
+    };
+    for (let i = 0; i < xAngry.length; i++) {
+        xAngry[i] += direction;
+    };
+
+    draw()
+
+    //Game-Over:
+    if (['angry', 'vAngry', 'xAngry'].some(c => spaces[currentHornIndex].classList.contains(c)))
+
+
+
+}
+
+neighboursId = setInterval(moveNeighbours, 500)
